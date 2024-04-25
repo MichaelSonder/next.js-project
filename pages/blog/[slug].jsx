0c533699fc63b55ce68f2b3f3b5f6b73f26e7c94
Michael Søndergaard <m.soendergaard87@gmail.com>
2024-04-25 10:59:25 +0200

    fixed styling

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,4 +1,5 @@
 import Image from "../../components/Image/Image";
+import Head from "next/head";
 import Link from "next/link";
 import { getAllSlugs, getPostData } from "../../lib/posts";
 import styles from "../../styles/BlogPost.module.css";
@@ -7,6 +8,9 @@ export default function BlogPost(props) {
   const { postData } = props;
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Food Blog</title>
+      </Head>
       <div style={{ maxWidth: "600px", marginTop: "20px" }}>
         <Image src={postData.coverImage} alt={postData.title} layout="fill" />
         <h1>{postData.title}</h1>
